9ced93d89a70f71d6d7ab08a7ef2735d210cdb5f
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2024-01-06 00:13:59 +0100

    Update target.mk

--- target/linux/mediatek/mt7622/target.mk
+++ target/linux/mediatek/mt7622/target.mk
@@ -2,7 +2,7 @@ ARCH:=aarch64
 SUBTARGET:=mt7622
 BOARDNAME:=MT7622
 CPU_TYPE:=cortex-a53
-DEFAULT_PACKAGES += mt7622 luci-app-mtwifi luci-app-upnp l1profile mt_wifi uboot-envtools
+DEFAULT_PACKAGES += mt7622 luci-app-mtwifi luci-app-upnp l1profile mt7622_mt_wifi uboot-envtools
 KERNELNAME:=Image dtbs
 
 define Target/Description
